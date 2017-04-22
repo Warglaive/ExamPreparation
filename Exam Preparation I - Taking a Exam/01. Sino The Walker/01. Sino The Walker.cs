95425465bf293296a31ff7221743ee4682e9c4bd
warglaivee <warglaivegm@gmail.com>
2017-04-22 20:33:33 +0300

    01. Sino The Walker written

---
+++ Exam Preparation I - Taking a Exam/01. Sino The Walker/01. Sino The Walker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _01.Sino_The_Walker
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputTime = Console.ReadLine();
+            var stepsTaken = int.Parse(Console.ReadLine()) % 86400; // deleno na 24 chasa
+            var secondsForStep = int.Parse(Console.ReadLine()) % 86400; //// deleno na 24 chasa
+            var time = DateTime.Parse(inputTime);
+
+            var totalTime = stepsTaken * secondsForStep;
+
+            var spanTime = TimeSpan.FromSeconds(totalTime);
+            var total = time + spanTime;
+            Console.WriteLine("Time Arrival: {0:HH:mm:ss}",total);
+        }
+    }
+}

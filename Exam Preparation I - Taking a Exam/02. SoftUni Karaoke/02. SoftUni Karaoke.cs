f5c2f35eed9134462a6a80bb3d3ff79a3b5ad842
warglaivee <warglaivegm@gmail.com>
2017-04-22 22:30:31 +0300

    02. SoftUni Karaoke started, to be written tomorrow.

---
+++ Exam Preparation I - Taking a Exam/02. SoftUni Karaoke/02. SoftUni Karaoke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SoftUni_Karaoke
+{
+  public  class Program
+    {
+       public static void Main()
+        {
+
+            var participants = Console.ReadLine().Split(new[] { ',', ' ' }
+            , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var song = Console.ReadLine().Split(new[] { ',',  }
+            , StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+            var stagePerformance = Console.ReadLine();
+            while (stagePerformance != "dawn") 
+            {
+                var token = stagePerformance.Split(new[] { ',', ' ' }
+            , StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+
+                var participant = token[0];
+                var songForParticipant = token[1];
+                var award = token[3];
+
+
+                stagePerformance = Console.ReadLine();
+            }
+        }
+    }
+}

3d6d5f9c4ccf99ab2ef31a203b7540bed53bdfbc
warglaive <warglaivegm@gmail.com>
2017-04-23 11:01:32 +0300

    01. Sweet Dessert written Laptop

--- Exam Preparation IV - Taking Exam/01. Sweet Dessert/01. Sweet Dessert.cs
+++ Exam Preparation IV - Taking Exam/01. Sweet Dessert/01. Sweet Dessert.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace _01.Sweet_Dessert
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+       public static void Main()
         {
+            var ivanchoCash = double.Parse(Console.ReadLine());
+            var guests = double.Parse(Console.ReadLine());
+            var bananaPrice = double.Parse(Console.ReadLine());
+            var eggsPrice = double.Parse(Console.ReadLine());
+            var berriesPrice = double.Parse(Console.ReadLine());
+
+            var setsAndPortions = Math.Ceiling(guests / 6);
+            var neededProducts = setsAndPortions * (2 * bananaPrice) + setsAndPortions * (4 * eggsPrice) + setsAndPortions * (0.2 * berriesPrice);
+            var neededMoney = neededProducts - ivanchoCash;
+            if (neededProducts<= ivanchoCash)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {neededProducts:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {neededMoney:f2}lv more.");
+            }
         }
     }
 }

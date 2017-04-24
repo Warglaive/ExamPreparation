831ad5fe8742dbf1b6ef7688ef7c006415f50649
warglaivee <warglaivegm@gmail.com>
2017-04-24 17:25:21 +0300

    90/100 Judge, fix incoming

--- Exam Preparation I - Taking a Exam/02. SoftUni Karaoke/02. SoftUni Karaoke.cs
+++ Exam Preparation I - Taking a Exam/02. SoftUni Karaoke/02. SoftUni Karaoke.cs
@@ -6,44 +6,71 @@ using System.Threading.Tasks;
 
 namespace _02.SoftUni_Karaoke
 {
-  public  class Program
+    public class Program
     {
-       public static void Main()
+        public static void Main()
         {
 
             var participants = Console.ReadLine().Split(new[] { ',', ' ' }
             , StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
 
-            var songs = Console.ReadLine().Split(new[] { ','}
-            , StringSplitOptions.RemoveEmptyEntries).Select(y => y.Trim())
-            .ToList();
-
+                var songs = Console.ReadLine().Split(new[] { ',' }
+                , StringSplitOptions.RemoveEmptyEntries).Select(y => y.Trim())
+                .ToList();
             var result = new Dictionary<string, List<string>>();
 
-            var stagePerformance = Console.ReadLine();
-            while (stagePerformance != "dawn")
+            if (participants.Count >= 1 && participants.Count <= 100 && songs.Count >= 1 && songs.Count <= 100) 
             {
 
-                var token = stagePerformance.Split(new[] { ',' }
-            , StringSplitOptions.RemoveEmptyEntries)
-                .ToList();
-
-
-
-                var participant = token[0].Trim();
-                var songForParticipant = token[1].Trim();
-                var award = token[2].Trim();
-                if (participants.Contains(participant) && songs.Contains(songForParticipant)) //possible bug
+                var stagePerformance = Console.ReadLine();
+                if (stagePerformance == "dawn")
                 {
-                    if (!result.ContainsKey(participant))
-                    {
-                        result.Add(participant, new List<string>());
-                        result[participant].Add(award);
-                    }
+                    Console.WriteLine("No awards");
+                    return;
                 }
+                while (stagePerformance != "dawn")
+                {
 
-                stagePerformance = Console.ReadLine();
+                    var token = stagePerformance.Split(new[] { ',' }
+                , StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                    var participant = token[0].Trim();
+                    var songForParticipant = token[1].Trim();
+                    var award = token[2].Trim();
+                    if (participants.Contains(participant) && songs.Contains(songForParticipant)) //possible bug
+                    {
+                        if (!result.ContainsKey(participant))
+                        {
+                            result.Add(participant, new List<string>());
+                        }
+                        if (!result[participant].Contains(award))
+                        {
+                            result[participant].Add(award);
+                        }
+                    }
+
+                    stagePerformance = Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No awards");
+                return;
+            }
+            foreach (var KvP in result
+                .OrderByDescending(x => x.Value.Count).ThenBy(y => y.Key)) 
+            {
+                if (KvP.Value.Contains(string.Empty))
+                {
+                    Console.WriteLine("No awards");
+                    break;
+                }
+                Console.WriteLine($"{KvP.Key}: {result[KvP.Key].Count} awards");
+                foreach (var award in KvP.Value.OrderBy(x => x)) 
+                {
+                    Console.WriteLine($"--{award}");
+                }
             }
         }
     }

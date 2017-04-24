75ef3d500100bbc0b3147353402866b87ae1f3b5
warglaivee <warglaivegm@gmail.com>
2017-04-24 17:27:39 +0300

    02. SoftUni Karaoke 90/100 judge

--- Exam Preparation I - Taking a Exam/02. SoftUni Karaoke/02. SoftUni Karaoke.cs
+++ Exam Preparation I - Taking a Exam/02. SoftUni Karaoke/02. SoftUni Karaoke.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.SoftUni_Karaoke
 {
@@ -10,12 +8,11 @@ namespace _02.SoftUni_Karaoke
     {
         public static void Main()
         {
-
             var participants = Console.ReadLine().Split(new[] { ',', ' ' }
             , StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
 
-                var songs = Console.ReadLine().Split(new[] { ',' }
+             var songs = Console.ReadLine().Split(new[] { ',' }
                 , StringSplitOptions.RemoveEmptyEntries).Select(y => y.Trim())
                 .ToList();
             var result = new Dictionary<string, List<string>>();
@@ -38,7 +35,7 @@ namespace _02.SoftUni_Karaoke
                     var participant = token[0].Trim();
                     var songForParticipant = token[1].Trim();
                     var award = token[2].Trim();
-                    if (participants.Contains(participant) && songs.Contains(songForParticipant)) //possible bug
+                    if (participants.Contains(participant) && songs.Contains(songForParticipant))
                     {
                         if (!result.ContainsKey(participant))
                         {

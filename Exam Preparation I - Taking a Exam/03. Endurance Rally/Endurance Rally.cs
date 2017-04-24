688a7c565255cf7b6751acd16681a4569f81b5a0
warglaivee <warglaivegm@gmail.com>
2017-04-24 22:09:00 +0300

    03. Endurance Rally written pc

--- Exam Preparation I - Taking a Exam/03. Endurance Rally/Endurance Rally.cs
+++ Exam Preparation I - Taking a Exam/03. Endurance Rally/Endurance Rally.cs
@@ -10,7 +10,36 @@ namespace _03.Endurance_Rally
     {
        public static void Main()
         {
+            var drivers = Console.ReadLine().Split().ToList();
+            var zones = Console.ReadLine().Split().Select(double.Parse).ToList();
+            var checkPoint = Console.ReadLine().Split().Select(int.Parse).ToList();
 
+            foreach (var driver in drivers)
+            {
+                double fuel = driver[0];
+
+                for (int i = 0; i < zones.Count; i++)
+                {
+                    if (checkPoint.Contains(i))
+                    {
+                        fuel += zones[i];
+                    }
+                    else
+                    {
+                        fuel -= zones[i];
+                    }
+                    if (fuel<=0)
+                    {
+                        Console.WriteLine($"{driver} - reached {i}");
+                        break;
+                    }
+                }
+                if (fuel<=0)
+                {
+                    continue;
+                }
+                Console.WriteLine($"{driver} - fuel left {fuel:f2}");
+            }
         }
     }
 }

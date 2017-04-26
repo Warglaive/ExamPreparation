43ee6d6ce05673e67bd08c3e21afdba7481b015d
warglaivee <warglaivegm@gmail.com>
2017-04-27 02:21:59 +0300

    02. Ladybugs too hardcore for me

--- Exam Preparation II - Taking a Exam/02. Ladybugs/02. Ladybugs.cs
+++ Exam Preparation II - Taking a Exam/02. Ladybugs/02. Ladybugs.cs
@@ -1,49 +1,134 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.Ladybugs
 {
-   public class Program
+    class LadyBugs100
     {
-       public static void Main()
+        static void Main(string[] args)
         {
-            var fieldZise = long.Parse(Console.ReadLine());
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] indexesWithBugs = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-            var cellsInTheField = new List<int>();
-            var ladyBugsIndexes = Console.ReadLine().Split().Select(long.Parse).ToList();
+            int[] field = new int[fieldSize];
 
-            for (int i = 0; i < fieldZise; i++)
+            for (int i = 0; i < fieldSize; i++)
             {
-                cellsInTheField.Add(0);
-            }
-            var commands = Console.ReadLine();
-
-            while (commands != "end") 
-            {
-                var inputParts = commands.Split().ToList();
-                var ladyBugIndex = int.Parse(inputParts[0]);
-                var direction = inputParts[1];
-                var flyLenght = int.Parse(inputParts[2]);
-                if (direction == "right") 
+                if (indexesWithBugs.Contains(i))
                 {
-                    ladyBugIndex += flyLenght;
-                    if (ladyBugIndex < fieldZise)
-                    {
+                    field[i] = 1;
+                }
+                else
+                {
+                    field[i] = 0;
+                }
+            }
 
-                        while (ladyBugsIndexes.Contains(ladyBugIndex))
+            string[] command = Console.ReadLine().Split(' ').ToArray();
+            while (command[0] != "end")
+            {
+                int index = int.Parse(command[0]);
+                int moving = int.Parse(command[2]);
+                if (index < 0 || index >= field.Length)
+                {
+                    command = Console.ReadLine().Split();
+                    continue;
+                }
+                else if (field[index] == 0)
+                {
+                    command = Console.ReadLine().Split();
+                    continue;
+                }
+                else if (field[index] == 1)
+                {
+                    if (command[1] == "right")
+                    {
+                        index = int.Parse(command[0]);
+                        moving = int.Parse(command[2]);
+
+                        if (moving > 0)
                         {
-                            ladyBugIndex += flyLenght;
+                            MoveRight(field, index, moving);
+                        }
+                        else if (moving < 0)
+                        {
+                            MoveLeft(field, index, Math.Abs(moving));
                         }
                     }
+
+                    else if (command[1] == "left")
+                    {
+                        if (moving > 0)
+                        {
+                            MoveLeft(field, index, moving);
+                        }
+                        else if (moving < 0)
+                        {
+                            MoveRight(field, index, Math.Abs(moving));
+                        }
+                    }
+
+                    command = Console.ReadLine().Split(' ').ToArray();
                 }
-                commands = Console.ReadLine();
+
+            } // End of the loop
+
+            Console.WriteLine(string.Join(" ", field));
+
+        } // End of Main method
+
+        static void MoveRight(int[] field, int index, int moving)
+        {
+
+            if (index + moving >= field.Length)
+            {
+                field[index] = 0;
             }
-            Console.Write(string.Join(" ", cellsInTheField));
-            
-           Console.WriteLine();
+            else
+            {
+                field[index] = 0;
+                for (int i = index + moving; i < field.Length; i += moving)
+                {
+                    if (field[i] == 1)
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        field[i] = 1;
+                        break;
+                    }
+                }
+            }
+            return;
+        }
+
+        static void MoveLeft(int[] field, int index, int moving)
+        {
+
+            if (index - moving < 0)
+            {
+                field[index] = 0;
+            }
+            else
+            {
+                field[index] = 0;
+
+                for (int i = index - moving; i > -1; i -= moving)
+                {
+                    if (field[i] == 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        field[i] = 1;
+                        break;
+                    }
+                }
+            }
+            return;
         }
     }
 }
